bdd006dd2e3ad03162e235876e3b373356070a2e
Esteban Pisani <estebandpisani@gmail.com>
2022-09-03 16:13:23 -0300

    profile s url fixed

--- routes.js
+++ routes.js
@@ -19,7 +19,7 @@ module.exports = function (app, myDataBase) {
         });
     });
     app.route('/profile').get(ensureAuthenticated, (req, res) => {
-        res.render(process.cwd() + 'views/pug/profile', {
+        res.render(process.cwd() + '/views/pug/profile', {
             username: req.user.username || 'error'
         })
     });
