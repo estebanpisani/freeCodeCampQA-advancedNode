05a9092e0cc79c4ffb0c7b3769584bcec5750b2e
Esteban Pisani <estebandpisani@gmail.com>
2022-09-03 12:06:57 -0300

    passport serialize setup

--- server.js
+++ server.js
@@ -25,6 +25,16 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
