1d8303d2708a1c670b7cdb698ecb3eeebd98d584
Esteban Pisani <estebandpisani@gmail.com>
2022-09-03 12:52:40 -0300

    form shown

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ myDB(async client => {
   app.route('/').get((req, res) => {
     res.render(process.cwd() + '/views/pug', {
       title: 'Connected to Database',
-      message: 'Please login'
+      message: 'Please login',
+      showLogin: true
     });
   });
 
