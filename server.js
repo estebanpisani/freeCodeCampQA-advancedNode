0f0e98a3a521eab5c4a11831af6e7909e13f3cf7
Esteban Pisani <estebandpisani@gmail.com>
2022-09-03 12:38:08 -0300

    db connection

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug', { title: 'Hello', message: 'Please login' });
-});
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
@@ -25,6 +22,9 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// app.route('/').get((req, res) => {
+//   res.render(process.cwd() + '/views/pug', { title: 'Hello', message: 'Please login' });
+// });
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
